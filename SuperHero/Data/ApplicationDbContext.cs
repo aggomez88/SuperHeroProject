de862595030a29de736db7088c706d5ee717fc96
Anthony Gomez <gomeztony004@gmail.com>
2020-02-14 15:14:41 -0600

    added model properties, added dbset, did initial migration, updated database, added controller without scaffolding

--- SuperHero/Data/ApplicationDbContext.cs
+++ SuperHero/Data/ApplicationDbContext.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperHero.Models;
 
 namespace SuperHero.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<SuperHeroModel> su { get; set; }
     }
 }
